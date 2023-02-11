26239fe1572c35461486986d94cec4de9dff15f9
ozark-dev <jason@ozarkdev.com>
2023-02-10 23:43:49 -0600

    form submission

--- src/index.js
+++ src/index.js
@@ -20,9 +20,40 @@ const books = [
 const BookList = () => {
   return (
     <section className='booklist'>
+      <EventExamples />
       {books.map((book) => {
         return <Book {...book} key={book.id} />;
       })}
+    </section>
+  );
+};
+
+const EventExamples = () => {
+  const handleFormInput = (e) => {
+    console.log(e.target);
+    console.log(e.target.name);
+    console.log(e.target.value);
+    console.log('this is the handleFormInput function');
+  };
+  //   const handleButtonClick = () => {
+  //     alert('handle form input');
+  //   };
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+  };
+  return (
+    <section>
+      <form onSubmit={handleFormSubmission}>
+        <h2>Example Form</h2>
+        <input
+          type='text'
+          name='example'
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+        <button type='submit'>Submit</button>
+      </form>
     </section>
   );
 };
