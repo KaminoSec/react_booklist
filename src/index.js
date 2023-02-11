466eb30e758ad7e76c5dc7e6ca05930a52b3d3ab
ozark-dev <jason@ozarkdev.com>
2023-02-10 23:07:21 -0600

    through passing object as prop

--- src/index.js
+++ src/index.js
@@ -2,37 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const books = [
+  {
+    author: 'Maximilian Schwarzmuller',
+    title: `React Key Concepts: Consolidate your knowledge of React's core features`,
+    img: './images/book1.jpg',
+    id: 1,
+  },
+  {
+    author: 'Robin Wieruch',
+    title: `The Road to React: Your journey to master React.js in JavaScript (2022 Edition)`,
+    img: 'https://m.media-amazon.com/images/I/41m+5+-JRNL.jpg',
+    id: 2,
+  },
+];
+
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
-};
-
-const Image = () => <img src='./images/book1.jpg' alt='React Key Concepts' />;
-const Title = () => (
-  <h2>
-    React Key Concepts: Consolidate your knowledge of React's core features
-  </h2>
-);
-const Author = () => {
-  return <h4>Maximilian Schwarzmuller</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
